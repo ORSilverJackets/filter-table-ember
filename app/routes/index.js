541dfc1b5c8677384fe5a0f03b850de9900278b5
Michael Koohafkan <Michael.C.Koohafkan@usace.army.mil>
2023-10-31 09:38:42 -0700

    fixes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,18 @@
 import Route from '@ember/routing/route';
 
-import jQuery from 'jquery';
+//import jQuery from 'jquery';
+//import Papa from 'papaparse'
 
-import Papa from "papaparse"
 export default class IndexRoute extends Route {
   async model() {
-    let response = await fetch("/api/test.json");
-    let parsed = await response.json();
+    let response = await fetch('/api/test.json');
+    let { data } = await response.json();
     
-    let response2 = await fetch("/api/program_selection_guide.csv");
-    let parsed2 = await response2.text();
-    let v = Papa.parse(parsed2)
+    //let response2 = await fetch('/api/program_selection_guide.csv');
+    //let parsed2 = await response2.text();
+    //let v = Papa.parse(parsed2)
 
-    return parsed.data ;
-
+    return data;
     //data.map((model) => {
     //  let { attributes } = model;
     //  // loop through offices and add to top level tags
