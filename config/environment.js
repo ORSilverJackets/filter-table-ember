a7b2b89d4f173e51fced1eff7ba97152313acebf
Michael Koohafkan <Michael.C.Koohafkan@usace.army.mil>
2023-11-06 13:55:25 -0800

    more fiddling with root url

--- config/environment.js
+++ config/environment.js
@@ -41,7 +41,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/filter-table-ember';
+    ENV.rootURL = '/filter-table-ember/';
     ENV.locationType = 'hash';
   }
 
